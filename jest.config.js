c56db51b240499f93bb91361f9f6bfaedde7cadd
+ <+>
2025-04-23 14:25:06 +0300

    changes

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,4 @@
+/** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: "jsdom", // Use jsdom for DOM-related tests
-  testEnvironmentOptions: {}, // Add this to avoid undefined errors
-  setupFilesAfterEnv: [
-    "@testing-library/jest-dom", // Automatically extend expect with jest-dom matchers
-  ],
-  moduleFileExtensions: ["js", "jsx"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
+  testEnvironment: "jsdom",
 };
