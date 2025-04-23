9ea714b87737b8f909f21c465a460eeb8e93ee1d
+ <+>
2025-04-23 08:55:35 +0300

    corrected

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setupTests.js"],
-  moduleFileExtensions: ["js", "jsx"],
-  testMatch: ["**/__tests__/**/*.test.js?(x)"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-};
+
+
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
