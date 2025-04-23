db93eeb00f584b03e954b50a1737362507ff3dbc
+ <+>
2025-04-23 15:16:10 +0300

    Update Jest config with explicit roots

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: "jsdom",
-  testMatch: ["<rootDir>/src/__tests__/**/*.test.js?(x)"],
+  roots: ["<rootDir>/src"], // Explicitly tell Jest where the source code (and potentially tests) are
+  testMatch: ["<rootDir>/src/**/__tests__/**/*.test.js?(x)"],
 };
