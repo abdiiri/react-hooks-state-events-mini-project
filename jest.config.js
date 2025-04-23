b668a297a8c7f0b535b2e2176decd402dbaba76d
+ <+>
2025-04-23 15:21:06 +0300

    Use explicit jest-environment-jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: "jsdom",
-  roots: ["<rootDir>/src"], // Explicitly tell Jest where the source code (and potentially tests) are
-  testMatch: ["<rootDir>/src/**/__tests__/**/*.test.js?(x)"],
+  testEnvironment: "jest-environment-jsdom",
+  testMatch: ["<rootDir>/src/__tests__/**/*.test.js?(x)"],
 };
