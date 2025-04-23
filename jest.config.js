7e1b1039501af92ee93f5c9378ceb1b567dfeed0
+ <+>
2025-04-23 09:23:04 +0300

    working

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // jest.config.js
 module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testEnvironment: "jest-environment-jsdom", // Specify the jsdom environment explicitly
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"], // Ensure this path is correct
   moduleFileExtensions: ["js", "jsx"],
   testMatch: ["**/__tests__/**/*.test.js?(x)"],
   transform: {
---
+++ src/setupTests.js
@@ -0,0 +1,2 @@
+// Enables custom jest matchers from React Testing Library
+import "@testing-library/jest-dom";
